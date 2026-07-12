9f470796651c4099e2391c3d12cf92d97eecd932
71im <71m5chulz@gmail.com>
2026-07-12 18:07:00 +0200

    testing JSON Parsing
    + addas parsing of json file, which is hostet on github

--- website.js
+++ website.js
@@ -2,7 +2,7 @@ const searchlist = document.getElementById("content-container");
 const searchbutton = document.getElementById("searchbutton");
 const searchinput = document.getElementById("searchinput");
 
-const marketJSONSource = "Test.json";
+const JSONURL = "https://raw.githubusercontent.com/notquitenull/notquitenull.github.io/refs/heads/main/data/Test.json"
 
 const ListOfMarkets = [];
 
@@ -37,8 +37,9 @@ function search(){
 function buildresults(){
 
     for(let i = 0; i< ListOfMarkets.length; i++ ){
+
         let divcontent = document.createElement("p");
-        let divtext = document.createTextNode("This is a test result "+ ListOfMarkets[i] );
+        let divtext = document.createTextNode("This is a test result "+ ListOfMarkets[i].getmarketid() +" "+  ListOfMarkets[i].getslope() +" " +ListOfMarkets[i].getdeviation() +" "+ ListOfMarkets[i].gettopics() );
         divcontent.appendChild(divtext);
 
         let newitem = document.createElement("div");
@@ -52,30 +53,35 @@ function buildresults(){
     
 }
 
-function loadMarketsFromJSON(){
+async function loadMarketsFromJSON(){
     console.log("TODO: loading Markets from JSON")
 
-    var MarketJSON = null;
-    fetch(marketJSONSource, {
-            method: 'POST',
-            mode: 'cors',
+    let MarketJSON = null;
+    
+    try{
+        const response = await fetch(JSONURL);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
         }
-    ).then(response => response.json()).then(value => console.log( value)).catch(error => console.log(error));
-    console.log(MarketJSON);
 
-    for(let i = 0; i < MarketJSON.length(); i++){
+        const result = await response.json();
 
-        console.log(MarketJSON[i].marketid);
-        let newmarket = market(MarketJSON[i].marketid,MarketJSON[i].avg_slope,MarketJSON[i].std_deviation,MarketJSON[i].topics);
+        MarketJSON = result;
+
+
+    }catch (error) {
+        console.error("error during fetch" + error.message);
+    }
+
+
+    for(let i = 0; i < MarketJSON.length; i++){
+
+        let newmarket = new market(MarketJSON[i].markt_id,MarketJSON[i].avg_slope,MarketJSON[i].std_deviation,MarketJSON[i].topics);
         
-
-        ListOfMarkets.push();
-
+        ListOfMarkets.push(newmarket);
     }
 }
 
-    
+loadMarketsFromJSON().then(buildresults);
 
 
-loadMarketsFromJSON();
-buildresults();
