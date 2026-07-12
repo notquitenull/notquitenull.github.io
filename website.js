2f006cc771ab6e756ef9c96d59dde43bcb1522d4
71im <71m5chulz@gmail.com>
2026-07-12 17:17:04 +0200

    added test.json
    added Test.JSON file for testing

--- website.js
+++ website.js
@@ -2,9 +2,32 @@ const searchlist = document.getElementById("content-container");
 const searchbutton = document.getElementById("searchbutton");
 const searchinput = document.getElementById("searchinput");
 
+const marketJSONSource = "Test.json";
+
 const ListOfMarkets = [];
 
 searchbutton.addEventListener("click",search);
+
+const market = class market {
+    constructor(marketid,avg_slope,std_deviation,topics){
+        this.marketid = marketid;
+        this.avg_slope = avg_slope;
+        this.std_deviation = std_deviation;
+        this.topics = topics;
+    }
+    getmarketid(){
+        return this.marketid;
+    }
+    getslope(){
+        return this.avg_slope;
+    }
+    getdeviation(){
+        return this.std_deviation;
+    }
+    gettopics(){
+        return this.topics;
+    }
+}
 
 
 function search(){
@@ -31,8 +54,27 @@ function buildresults(){
 
 function loadMarketsFromJSON(){
     console.log("TODO: loading Markets from JSON")
-    ListOfMarkets.push("test 1","test 2", "Test3");
+
+    var MarketJSON = null;
+    fetch(marketJSONSource, {
+            method: 'POST',
+            mode: 'cors',
+        }
+    ).then(response => response.json()).then(value => console.log( value)).catch(error => console.log(error));
+    console.log(MarketJSON);
+
+    for(let i = 0; i < MarketJSON.length(); i++){
+
+        console.log(MarketJSON[i].marketid);
+        let newmarket = market(MarketJSON[i].marketid,MarketJSON[i].avg_slope,MarketJSON[i].std_deviation,MarketJSON[i].topics);
+        
+
+        ListOfMarkets.push();
+
+    }
 }
+
+    
 
 
 loadMarketsFromJSON();
